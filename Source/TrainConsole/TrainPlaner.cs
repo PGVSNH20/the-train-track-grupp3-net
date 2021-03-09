04dad25e988813dff3c62832bb62b2e0c2601f14
andreasbrunnstrom <best_i25@hotmail.com>
2021-03-09 15:00:43 +0100

    Create TrainPlaner
    Created class and implement Interface.

---
+++ Source/TrainConsole/TrainPlaner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainEngine;
+
+namespace TrainConsole
+{
+    class TrainPlaner : ITravelPlan
+    {
+        public List<object> TimeTable => throw new NotImplementedException();
+
+        public object Train => throw new NotImplementedException();
+
+        public void Load(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
