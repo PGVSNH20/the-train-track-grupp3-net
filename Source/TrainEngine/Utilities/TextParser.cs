dd47687e2a00a9367427d72fd8a5769d8e360a20
TooOldForThisGit <j.o.andersson@hotmail.se>
2021-03-10 12:52:42 +0100

    Folder structure for objects and utilizing TextParser

---
+++ Source/TrainEngine/Utilities/TextParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine.Utilities
+{
+    public class TextParser
+    {
+
+        public static string[] GetData(string pathToFile)
+        {
+            string[] dataRow = { "rad1", "rad2", "rad3" };
+            //using (resource)
+            //{
+            // stream here and split rows
+            //}
+
+            return dataRow; 
+        }
+    }
+}
