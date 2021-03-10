4c85c7f727e928ecf398d3aca92d51170d883c85
Magnusson <Magnusson-5@hotmail.com>
2021-03-10 13:00:51 +0100

    Added reader to project

--- Source/TrainEngine/Reader/FileReader.cs
+++ Source/TrainEngine/Reader/FileReader.cs
@@ -12,14 +12,13 @@ namespace TrainEngine.Reader
         {
             try
             {
-                // Create an instance of StreamReader to read from a file.
-                // The using statement also closes the StreamReader.
-                using (StreamReader sr = new StreamReader("../../../Data/passengers.txt"))
+                // Startar stream, letar rätt på fil
+                using (StreamReader streamReader = new StreamReader(@"C:\Users\magnu\OneDrive\Skrivbord\.NetUtveckling\Dataåtkomster i .NET\Gruppuppgift\TheTrainTrack\Source\TrainEngine\Reader\TxtFiles\passengers.txt"))
                 {
                     string line;
-                    // Read and display lines from the file until the end of
-                    // the file is reached.
-                    while ((line = sr.ReadLine()) != null)
+
+                    // Läser filen & skriver ut
+                    while ((line = streamReader.ReadLine()) != null)
                     {
                         Console.WriteLine(line);
                     }
@@ -27,7 +26,6 @@ namespace TrainEngine.Reader
             }
             catch (Exception e)
             {
-                // Let the user know what went wrong.
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
