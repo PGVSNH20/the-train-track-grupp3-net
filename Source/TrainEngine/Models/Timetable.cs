eb1dc9fcc8fd7b14447f2d988cf7f20a48fb70f8
TooOldForThisGit <j.o.andersson@hotmail.se>
2021-03-10 18:23:14 +0100

    Made timetable class structure

---
+++ Source/TrainEngine/Models/Timetable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine.Models
+{
+    class Timetable
+    {
+        /*        
+            TraindId,StationId,DepartureTime,ArrivalTime
+            2,1,10:20,null
+            2,2,10:45,10:43
+            2,3,null,10:59
+            3,3,10:23,null
+            3,4,10:55,10:53
+            3,1,null,11:15
+         */
+        public int Id { get; set; }
+        public int StationId { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+
+    }
+}
